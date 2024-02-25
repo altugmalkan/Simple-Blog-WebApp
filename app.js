550acec1cb35d5bcaffca81f63612bbf7d26c574
Altug Malkan <altug33@windowslive.com>
2024-02-25 18:30:48 +0300

    Update: Implemented functionality to display posted messages on the 'posts.ejs' page.

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+class Kisi{
+    constructor(name,email,text){
+        this.name = name;
+        this.email = email;
+        this.text = text;
+    }
+}
+const kisiler = [];
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    const {name,email,text} = req.body;
+    const yeniKisi = new Kisi(name,email,text);
+    kisiler.push(yeniKisi);
+    res.render("posts.ejs", {kisiler: kisiler, name: yeniKisi.name, email: yeniKisi.email, text: yeniKisi.text});
+});
+app.get("/kisiler", (req, res) => {
+    // Tüm kişilerin bilgilerini JSON formatında yanıt olarak gönderin
+    res.json(kisiler);
 });
 
 
